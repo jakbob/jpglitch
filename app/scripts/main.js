e23dccbd2e37ef82ae8f9d94f229fdf849a45386
Jakob Florell <jakob@cp80.se>
2015-06-03 23:51:44 +0200

    Parsning av vanliga markers
    Vi hittar DQT-sektionen och får ut vettig data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,42 @@
 'use strict';
 
+function parseJpeg(arrayBuffer) {
+	var data = new DataView(arrayBuffer);
+	var i = 0;
+	while(i < data.byteLength) {
+		var marker = data.getUint16(i);
+		i += 2;
+		switch(marker) {
+			case 0xffd8:
+				console.log('start of image');
+				break;
+			case 0xffe0:
+			case 0xffe1:
+			case 0xffed:
+			case 0xffc0:
+			case 0xffc4:
+				console.log('found marker', marker.toString(16));
+				var length = data.getUint16(i);
+				i += length;
+				break;
+			case 0xffdB:
+				var dqtLength = data.getUint16(i);
+				var dqtId = data.getUint8(i+2);
+				var dqt = new Uint8Array(arrayBuffer.slice(i+3, i+dqtLength));
+
+				console.log('found DQT!!!!! Position:', i, 'id:', dqtId, 'Length:', dqt.length, dqt);
+				i += data.getUint16(i);
+				break;
+			case 0xffda:
+				console.log('Found Start of Scan. The rest is entropy encoded data, aborting...');
+				return;
+			default:
+				console.log('Unknown maker:', marker.toString(16));
+				return;
+		}
+	}
+}
+
 function handleFileSelect(evt) {
 	var files = evt.target.files; // FileList object
 
@@ -10,7 +47,8 @@ function handleFileSelect(evt) {
 		reader.onload = (function() {
 			return function(e) {
 				var raw = e.target.result;
-				// https://developer.mozilla.org/en/JavaScript_typed_arrays
+				parseJpeg(raw);
+
 				var rawBytes = new Uint8Array(raw);
 
 				var stringView = new StringView(rawBytes);
