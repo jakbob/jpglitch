c7c2441f6cba1f5d83f03f61b3af5695dd9e6d0d
Jakob Florell <jakob@cp80.se>
2018-09-10 23:11:58 +0200

    WIP Zigzag transform DQT

--- src/app/components/editor.vue
+++ src/app/components/editor.vue
@@ -42,7 +42,11 @@ export default {
   watch: {
     imageFile(value) {
       if (!value) { return; }
-      this.dqts = JPGHelper.parseQuantizationTables(value);
+      const dqts = JPGHelper.parseQuantizationTables(value);
+      this.dqts = dqts.map(dqt => ({
+        ...dqt,
+        data: this.zigzagDqt(dqt.data)
+      }));
       this.rawImage = new Uint8Array(value);
     }
   },
@@ -50,9 +54,47 @@ export default {
     dqtUpdated(dqt, index) {
       Vue.set(this.dqts, index, dqt);
 
+
       var raw = this.rawImage.subarray(0);
       raw.set(dqt.data, dqt.position);
       this.rawImage = raw;
+    },
+    zigzagDqt(dqt) {
+      const reordered = new Array(dqt.length);
+      let reorderedX = 0, reorderedY = 0;
+      let direction = 'up';
+
+      for (let dqtIndex = 0; dqtIndex < dqt.length; dqtIndex++) {
+        reordered[reorderedX + reorderedY * 8] = dqt[dqtIndex];
+
+        if (direction === 'up') {
+          if (reorderedY === 0) {
+            reorderedX += 1;
+            direction = 'down';
+          } else if (reorderedX === 7) {
+            reorderedY += 1;
+            direction = 'down';
+          } else {
+            reorderedX += 1;
+            reorderedY -= 1;
+          }
+        } else {
+          if (reorderedY === 7) {
+            reorderedX += 1;
+            direction = 'up';
+          } else if (reorderedX === 0) {
+            reorderedY += 1;
+            direction = 'up';
+          } else {
+            reorderedX -= 1;
+            reorderedY += 1;
+          }
+        }
+      }
+
+      console.log(reordered);
+
+      return reordered;
     }
   }
 };
